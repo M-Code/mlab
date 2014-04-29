cd2ac191d9fefab6e40ef88c599726247c83d72a
Ming Liu <mliu@otcmarkets.com>
2014-04-29 19:27:44 -0400

    problem 31.

---
+++ src/main/java/com/mlab/concurrency/VolatileLab.java
@@ -0,0 +1,36 @@
+package com.mlab.concurrency;
+
+/**
+ * Volatile variables are not thread-safe!
+ */
+public class VolatileLab {
+	public volatile int a = 0;
+
+	public static void main(String[] args) throws Exception {
+		final VolatileLab vol = new VolatileLab();
+		
+		Thread t1 = new Thread(new Runnable() {
+
+			public void run() {
+				for(int i = 0; i < 100000; i++)
+					vol.a = vol.a + 1;
+			}
+			
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				for(int i = 0; i < 100000; i++)
+					vol.a = vol.a + 1;
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println("a == " + vol.a);
+
+	}
+
+}
---
+++ src/main/java/com/mlab/euler/Problem31.java
@@ -0,0 +1,41 @@
+package com.mlab.euler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem31 {
+	private Set<Integer> money = new HashSet<Integer>();
+	public Problem31() {
+		money.add(1);
+		money.add(2);
+		money.add(5);
+		money.add(10);
+		money.add(20);
+		money.add(50);
+		money.add(100);
+		money.add(200);
+	}
+	
+	public int ways(int currentVal, int add, int goal) {
+		if(currentVal == goal) {
+			return 1;
+		} else if(currentVal > goal) {
+			return 0;
+		} else {
+			int sum = 0;
+			for(Integer m : money) {
+				if(m >= add)
+					sum += ways(currentVal + m, m, goal);
+			}
+			return sum;
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		Problem31 prob = new Problem31();
+		System.out.println(prob.ways(0, 0, 200));
+	}
+
+}
